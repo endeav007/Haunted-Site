3e498c605a415f1ff899deda8fa242b6c9d34cec
endeav007 <autumnaustin046@gmail.com>
2025-12-01 16:24:07 -0800

    Started Main Site CSS

---
+++ haunted-site/src/MainSite.jsx
@@ -0,0 +1,26 @@
+
+import './styles.css'
+
+function MainPage(){
+
+    return (
+    <>
+        <div className="landing-page"> 
+            <div className="title-image"> 
+                <div className = "logo">
+                    <h3> Dream </h3>
+                    <h1> BIG! </h1>
+                </div>
+            </div>
+            <div className="learn-more">
+                    <h1> Learn More !</h1>
+                    <button> yess </button>
+            </div>
+        </div>
+    </>
+        
+    );
+}
+
+
+export default MainPage;
--- haunted-site/src/main.jsx
+++ haunted-site/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles.css'
 import App from './App.jsx'
 import SwapImage from './ImageSwap.jsx'
 import Scene from './TransitionScene.jsx'
+import MainPage from './MainSite.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Scene />
-    <SwapImage imagePrefix={"./Image1"}/>
+    <MainPage />
   </StrictMode>,
 )
